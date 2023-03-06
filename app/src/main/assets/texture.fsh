329c695c26f327c0daf9b22f8aa704d203f75c19
lipengfei <1005904057@qq.com>
2023-03-06 13:13:56 +0800

    纹理完成

--- app/src/main/assets/texture.fsh
+++ app/src/main/assets/texture.fsh
@@ -4,7 +4,8 @@ out vec4 fragColor;
 in vec3 outColor;
 in vec2 TexCoord;
 uniform sampler2D texture1;
+uniform sampler2D texture2;
 void main()
 {
-    fragColor = texture(texture1, TexCoord);
+    fragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(1.0 - TexCoord.x, TexCoord.y)), 0.5);;
 }
--- pplayer/src/main/cpp/gles/PictureSample.cpp
+++ pplayer/src/main/cpp/gles/PictureSample.cpp
@@ -6,14 +6,15 @@
 #include "MyAssetManager.h"
 
 unsigned int texture;
+unsigned int texture2;
 
 void PictureSample::init() {
     m_iProgramObj = GLTools::initProgram("texture.vsh", "texture.fsh");
     float vertices[] = {
             //position  //color //texture
-            0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-            0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
-            -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+            0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 2.0f, 0.0f,
+            0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 2.0f, 2.0f,
+            -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 2.0f,
             -0.5f, 0.5f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f
     };
     unsigned int indices[] = {
@@ -43,8 +44,8 @@ void PictureSample::init() {
     glEnableVertexAttribArray(2);
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     int width = 0;
@@ -53,15 +54,44 @@ void PictureSample::init() {
     long fileLength;
     unsigned char * fileData = MyAssetManager::getInstance()->readPicture("img/container.jpg", fileLength);
     unsigned char* image = stbi_load_from_memory(fileData, fileLength, &width, &height, &nrChannels, 0);
-    delete[] fileData;
     LOGE(TAG, "图片宽高 %d %d" , width, height)
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
     glGenerateMipmap(GL_TEXTURE_2D);
-    delete[] image;
+    stbi_image_free(image);
+    delete[] fileData;
+
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width2 = 0;
+    int height2 = 0;
+    int nrChannels2 = 0;
+    long fileLength2;
+    unsigned char * fileData2 = MyAssetManager::getInstance()->readPicture("img/awesomeface.png", fileLength2);
+    unsigned char *image2 = stbi_load_from_memory(fileData2, fileLength2, &width2, &height2, &nrChannels2, 0);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width2, height2, 0, GL_RGBA, GL_UNSIGNED_BYTE, image2);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    LOGE(TAG, "图片宽高 %d %d" , width2, height2)
+    stbi_image_free(image2);
+    delete[] fileData2;
+
+    glUseProgram(m_iProgramObj);
+    glUniform1i(glGetUniformLocation(m_iProgramObj, "texture1"), 0);
+    glUniform1i(glGetUniformLocation(m_iProgramObj, "texture2"), 1);
+
+    //glEnable(GL_BLEND); // 开启颜色混合
+    //glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); // 设置颜色混合模式
 }
 
 void PictureSample::draw(int width, int height) {
+    glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, texture);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
     glUseProgram(m_iProgramObj);
     glBindVertexArray(VAO);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
